de62a56defde89b42395fd14b9be164bd5a8f743
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-10-06 13:22:41 +0530

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import "./component_style/about.css";
 
 const About = () => {
   return (
@@ -9,7 +10,16 @@ const About = () => {
       exit={{ width: 0, opacity: 0 }}
       transition={{ type: "tween" }}
     >
-      <h1>this will be about</h1>
+      <div className="card">
+        A web developer who is proficient in both <span>frontend</span> and <span>backend</span>. My passion lies in learning new technologies.
+      </div>
+      <div className="card">
+        An undergraduate engineering student in Electronics and Computer Science from KIIT University, Bhubaneshwar.
+      </div>
+      <div className="buttons">
+        <span className="btn-1">Internships</span>
+        <span className="btn-2">Hackathons</span>
+      </div>
     </motion.div>
   );
 };
